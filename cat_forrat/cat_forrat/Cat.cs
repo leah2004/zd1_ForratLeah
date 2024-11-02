4ec148a23587968281862303116db5f158d59082
leah2004 <ayadeer36@gmail.com>
2024-11-02 11:08:31 +0500

    Add files via upload

---
+++ cat_forrat/cat_forrat/Cat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat_forrat
+{
+    internal class Cat
+    {
+        private string name; //скрытое поле
+        private double ves;
+
+        public string Name // свойство
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return name;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+
+        public double Ves {
+            get {
+                return ves;          
+            }
+            set {
+
+                if (value > 0) //проверяем утсановленное значение
+                {
+                    ves = value; //устанвливаем вес если проходит проверку
+                    Meow();
+                }
+                else {
+                    Console.WriteLine($" неправильный вес!!!"); // сообщение об ошибке
+
+                }              
+           }
+        
+        }
+            
+        public void Meow()// вывод на экарн информации о животном
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!! ------ вес данного животного {ves} кг");
+        }
+    }
+}
---
+++ cat_forrat/cat_forrat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat_forrat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Cat catt = new Cat();
+                Console.WriteLine("введите имя коту");
+                string name = Console.ReadLine();
+                catt.Name = name;
+                Console.WriteLine("введите вес коту");
+                double ves = Convert.ToDouble(Console.ReadLine());
+               
+                    catt.Ves = ves;
+                
+               
+            }
+            catch(FormatException){ Console.WriteLine("неправильно введены данные"); }
+
+            Console.ReadKey();
+
+        }
+    }
+}
